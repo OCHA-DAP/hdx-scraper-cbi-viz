63e48c4d91af0bc6c034051078c6137731173a5d
Mike <rans@un.org>
2022-08-30 16:19:23 +1200

    Update HDX Python API Use function in utilities to write output

--- cbi.py
+++ cbi.py
@@ -1,59 +1,10 @@
-import json
 import logging
-from os.path import join
 
-import unicodecsv
 from hdx.data.dataset import Dataset
+from hdx.utilities.saver import save_hxlated_output
 from hdx.utilities.text import get_numeric_if_possible
 
 logger = logging.getLogger(__name__)
-
-
-def write(today, output_dir, configuration, configuration_key, rows, skipped=None):
-    logger.info(f"Writing {configuration_key} files to {output_dir}")
-    file_configuration = configuration[configuration_key]
-    headers = file_configuration["headers"]
-    hxltags = file_configuration["hxltags"]
-    process_cols = file_configuration.get("process_cols", dict())
-    csv_configuration = file_configuration["csv"]
-    json_configuration = file_configuration["json"]
-    csv_hxltags = csv_configuration.get("hxltags", hxltags)
-    json_hxltags = json_configuration.get("hxltags", hxltags)
-    hxltag_to_header = dict(zip(hxltags, headers))
-    csv_headers = [hxltag_to_header[hxltag] for hxltag in csv_hxltags]
-    metadata = {"#date+run": today, f"#meta+{configuration_key}+num": len(rows)}
-    if skipped is not None:
-        metadata[f"#meta+{configuration_key}+skipped+num"] = skipped
-    metadata_json = json.dumps(metadata, indent=None, separators=(",", ":"))
-    with open(join(output_dir, csv_configuration["filename"]), "wb") as output_csv:
-        writer = unicodecsv.writer(output_csv, encoding="utf-8", lineterminator="\n")
-        writer.writerow(csv_headers)
-        writer.writerow(csv_hxltags)
-        with open(join(output_dir, json_configuration["filename"]), "w") as output_json:
-            output_json.write(f'{{"metadata":{metadata_json},"data":[\n')
-
-            def write_row(inrow, ending):
-                def get_outrow(file_hxltags):
-                    outrow = dict()
-                    for file_hxltag in file_hxltags:
-                        expression = process_cols.get(file_hxltag)
-                        if expression:
-                            for i, hxltag in enumerate(hxltags):
-                                expression = expression.replace(hxltag, f"inrow[{i}]")
-                            outrow[file_hxltag] = eval(expression)
-                        else:
-                            outrow[file_hxltag] = inrow[hxltags.index(file_hxltag)]
-                    return outrow
-
-                writer.writerow(get_outrow(csv_hxltags).values())
-                row = get_outrow(json_hxltags)
-                output_json.write(
-                    json.dumps(row, indent=None, separators=(",", ":")) + ending
-                )
-
-            [write_row(row, ",\n") for row in rows[:-1]]
-            write_row(rows[-1], "]")
-            output_json.write("}")
 
 
 def start(configuration, today, retriever, output_dir):
@@ -141,43 +92,59 @@ def start(configuration, today, retriever, output_dir):
 
     outputs_configuration = configuration["outputs"]
 
+    # def write(today, output_dir, configuration, configuration_key, rows, skipped=None):
     # Prepare and write flows
-    write(
-        today,
-        output_dir,
-        outputs_configuration,
-        "flows",
-        [
-            flows[key]["row"]
-            + [int(round(flows[key]["value"]))]
-            + [flows[key]["description"]]
-            for key in flows
-        ],
+    logger.info(f"Writing flows files to {output_dir}")
+    out_flows = [
+        flows[key]["row"]
+        + [int(round(flows[key]["value"]))]
+        + [flows[key]["description"]]
+        for key in flows
+    ]
+    save_hxlated_output(
+        outputs_configuration["flows"],
+        out_flows,
+        includes_header=False,
+        includes_hxltags=False,
+        output_dir=output_dir,
+        today=today,
+        num_flows=len(out_flows),
     )
 
     # Write transactions
-    write(
-        today,
-        output_dir,
-        outputs_configuration,
-        "transactions",
+    logger.info(f"Writing transactions files to {output_dir}")
+    save_hxlated_output(
+        outputs_configuration["transactions"],
         transactions,
+        includes_header=False,
+        includes_hxltags=False,
+        output_dir=output_dir,
+        today=today,
+        num_transactions=len(transactions),
     )
 
     # Write reporting orgs
-    write(
-        today,
-        output_dir,
-        outputs_configuration,
-        "reporting_orgs",
-        [t[::-1] for t in sorted(provider_name_to_id.items())],
+    logger.info(f"Writing reporting orgs files to {output_dir}")
+    reporting_orgs = [t[::-1] for t in sorted(provider_name_to_id.items())]
+    save_hxlated_output(
+        outputs_configuration["reporting_orgs"],
+        reporting_orgs,
+        includes_header=False,
+        includes_hxltags=False,
+        output_dir=output_dir,
+        today=today,
+        num_reporting_orgs=len(reporting_orgs),
     )
 
     # Write receiver orgs
-    write(
-        today,
-        output_dir,
-        outputs_configuration,
-        "receiver_orgs",
-        [t[::-1] for t in sorted(receiver_name_to_id.items())],
+    logger.info(f"Writing receiver orgs files to {output_dir}")
+    receiver_orgs = [t[::-1] for t in sorted(receiver_name_to_id.items())]
+    save_hxlated_output(
+        outputs_configuration["receiver_orgs"],
+        receiver_orgs,
+        includes_header=False,
+        includes_hxltags=False,
+        output_dir=output_dir,
+        today=today,
+        num_receiver_orgs=len(receiver_orgs),
     )
